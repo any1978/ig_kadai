201858b0955ef7476abd034530b6982b9150207f
月野木勝洋 <tsukinokikatsuhiro@gatsunogikatsuhironoMacBook-Air.local>
2020-03-29 02:02:20 +0900

    本日終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :feeds, only: [:new, :create, :destroy]
+  # resources :feeds, only: [:new, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :favorites, only: [:create, :destroy]
+  # resources :favorites, only: [:create, :destroy]
   resources :blogs do
     collection do
       post :confirm
